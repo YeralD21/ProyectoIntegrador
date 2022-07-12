2b0b6b533ba10f7aede2ff6e624d56661989e9e4
YeralD21 <yesarbot14@gmail.com>
2022-07-12 16:18:43 -0500

    Proyecto Integrador

--- SysVentasG2/src/main/java/pe/edu/upeu/app/MenuOpciones.java
+++ SysVentasG2/src/main/java/pe/edu/upeu/app/MenuOpciones.java
@@ -1,11 +1,19 @@
 package pe.edu.upeu.app;
 
 import java.io.Console;
+import java.util.Scanner;
 
+import pe.edu.upeu.dao.CategoriaDao;
+import pe.edu.upeu.dao.ClienteDao;
+import pe.edu.upeu.dao.MarcaDao;
+import pe.edu.upeu.dao.ProductoDao;
+import pe.edu.upeu.dao.UsuarioDao;
+import pe.edu.upeu.dao.VentaDao;
 import pe.edu.upeu.modelo.UsuarioTO;
 import pe.edu.upeu.util.LeerTeclado;
 
 public class MenuOpciones {
+    Scanner sc=new Scanner (System.in);
     LeerTeclado leerT = new LeerTeclado();
     UsuarioTO uTo = new UsuarioTO();
     Console cons = System.console();
@@ -17,8 +25,7 @@ public class MenuOpciones {
         System.out.println("Ingrese su clave:");
         char[] clavex = cons.readPassword();
         uTo.clave = String.valueOf(clavex);
-
-        if (uTo.usuario.equals("davidmp") && uTo.clave.equals("123456")) {
+        if (new UsuarioDao().login(uTo.getUsuario(), new UsuarioDao().getMD5EncryptedValue(uTo.getClave()))) {
             menuPrincipal();
         } else {
             System.out.println("Intente Nuevamente!! sus credenciales son invalidos!");
@@ -28,19 +35,31 @@ public class MenuOpciones {
     
     public void menuPrincipal() {
         System.out.println("------------------------------------Sistema de Ventas------------------------------");
-        int opciones = 1;
+    
         String menu = "";
-        menu += "1=Registro de Productos\n";
-        menu += "2=Registro de Ventas\n";
+        menu += "1=Registro de Usuario\n";
+        menu += "2=Listar Usuarios\n";
+        menu += "3=Crear Categoria\n";
+        menu += "4=Crear Marca\n";
+        menu += "5=Crear Cliente\n6=Crear Producto\n7=Listar Productos\n";
+        menu +="8=Registro Ventas\n9=Reporte de Ventas";
         System.out.println(menu);
+        int opciones = sc.nextInt();
         do {
             switch (opciones) {
-                case 1: System.out.println("Registro Productos");   break;
-
+                case 1: new UsuarioDao().crearUsuarios();   break;
+                case 2: new UsuarioDao().listarUsuarios();   break;
+                case 3: new CategoriaDao().crearCategoria();  break;
+                case 4: new MarcaDao().crearMarca(); break;
+                case 5: new ClienteDao().crearCliente(); break;
+                case 6: new ProductoDao().crearProducto(); break;
+                case 7: new ProductoDao().reportarProductos(); break;
+                case 8: new VentaDao().registroVentaGeneral();break;
+                case 9: new VentaDao().reporteVentasRangoFecha();break;
                 default: System.out.println("Opcion Invalida!"); break;
             }
             String continuar=leerT.leer("",
-             "Desea Continuar? S=Si/N=No").charAt(0)=='S'?"S":"N";
+             "Desea Continuar? S=Si/N=No").toUpperCase().charAt(0)=='S'?"S":"N";
             if (continuar.charAt(0)=='S') {
                 opciones=leerT.leer(0, "Eliga una operacion:\n"+menu);
             }else{opciones=0;}
